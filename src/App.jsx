b6e3e0aaa30ec0201a5b31a1a404380bb108ffb7
dharam singh <rawatdharam861@gmail.com>
2024-10-03 01:48:52 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-const App=()=>{
-const display=()=>{
-    alert(" thi is btn : ")
+const App =()=>{
+    const myname=(nm,e)=>{
+        let name=e.target.name;
+        let val=e.target.value;
+        alert("name :"+name+ " value :"+val);
+       
 
-}
-
+    }
     return(
         <>
-        <h1>Welcome to reactm class in cybrom </h1>
-        <button onClick={display}>click</button>
+        <h1>Dharam Rawat</h1>
+        <button name="btn" value="mybtn1" onClick={(e)=>{myname("Dharam singh",e)}}> show data</button>
         </>
-
     )
 }
 export default App;
