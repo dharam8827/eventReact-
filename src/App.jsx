413fe4be2089205ab288794a6e8ef2aa07e3359e
dharam singh <rawatdharam861@gmail.com>
2024-10-03 01:25:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App=()=>{
+const display=()=>{
+    alert(" thi is btn : ")
+
+}
+
+    return(
+        <>
+        <h1>Welcome to reactm class in cybrom </h1>
+        <button onClick={display}>click</button>
+        </>
+
+    )
+}
+export default App;
